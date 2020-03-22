bb95e441d2145799cbf22e0bbb4d0ae9fa8ba7a1
alwx <alwxndr@gmail.com>
2020-03-22 13:08:12 +0100

    Update

--- index.js
+++ index.js
@@ -1,15 +1,52 @@
+const _ = require('lodash');
 const core = require('@actions/core');
-const github = require('@actions/github');
+const {request} = require("@octokit/request");
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+const token = core.getInput("token");
+const column_id = core.getInput("column_id");
+const archived_state = core.getInput("archived_state");
+const per_page = core.getInput("per_page");
+const page = core.getInput("page");
+
+const previews = [
+  "inertia",
+];
+
+const inputs = {
+  token,
+  column_id,
+  archived_state,
+  per_page,
+  page,
+};
+
+async function octionRequest(
+  token,
+  method,
+  path,
+  previews,
+  inputs
+) {
+  const requestWithAuth = request.defaults({
+    headers: {
+      authorization: `Bearer ${token}`
+    },
+    mediaType: {
+      previews
+    }
+  });
+  return await requestWithAuth(`${method} ${path}`, inputs);
 }
+
+octionRequest(token,
+  "get",
+  "/projects/columns/{column_id}/cards",
+  previews,
+  _.omit(inputs, ["token", "file_output", "custom_outputs"])
+).then(result => {
+  console.log("ALWX RESULT", result);
+})
+  .catch(error => {
+    console.log("error", error);
+    core.setFailed(error.message);
+  });
