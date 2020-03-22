24eeff761a256fb6fe4edd647835b5d06b85a42b
alwx <alwxndr@gmail.com>
2020-03-22 13:57:32 +0100

    Cards yay

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const column_id = core.getInput("column_id");
 const archived_state = core.getInput("archived_state");
 const per_page = core.getInput("per_page");
 const page = core.getInput("page");
+const order = core.getInput("order").split(',');
 
 const inputs = {
   token,
@@ -32,8 +33,12 @@ function getIssueForCard(card) {
   return performRequest({
     token,
     path: `GET ${card['content_url'].replace('https://api.github.com', '')}`,
-    inputs: _.omit(inputs, ["token"]),
+    inputs: _.omit(inputs, ["token"])
   })
+}
+
+function moveCards(cards) {
+  console.log(cards);
 }
 
 function getCards() {
@@ -47,15 +52,15 @@ function getCards() {
         return card['content_url'] != null;
       }).map(card => {
         return getIssueForCard(card).then(issue => {
-          card.issue = issue['data'];
+          card.labels = issue['data']['labels'];
           return card;
         });
       });
 
-      Promise.all(promises).then(results => {
-        console.log(results);
-      }).catch(e => {
-        console.error(e);
+      Promise.all(promises).then(cards => {
+        moveCards(_.sortBy(cards, (v) => order.indexOf(v.labels[0])));
+      }).catch(error => {
+        core.setFailed(error.message);
       });
     }
   }).catch(error => {
